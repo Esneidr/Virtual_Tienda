dd174b0417dde8d4989cbda304b5ceb598e773c6
Esneidr <eacordobam@gmail.com.com>
2022-11-20 23:40:18 -0500

    Segundo Commit

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Productos(models.Model):
+    nombre_producto = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=30)
+    precio = models.IntegerField(default=0)
+    cantidad = models.IntegerField(default=0)
+    descripcion = models.TextField()
--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from inventario.models import Productos
+from django.http import HttpResponse
 
 # Create your views here.
+
+def busqueda_producto(request):
+    return render(request, "buscar_producto.html")
